f0fc1fd3454b48fbfe85714aec0068046b710972
Zanqi Liang <zanqi.liang@gmail.com>
2013-06-30 18:57:03 -0400

    Finshi user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password_confirmation, presence: true
   validates :password, length: {minimum: 6}
-
+  self.per_page = 10
+  
   private
 
     def create_remember_token
